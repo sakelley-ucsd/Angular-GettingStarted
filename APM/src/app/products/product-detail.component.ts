c553ac3462d40f625d9138be3d6b903ba5715e1b
Sarah Kelley <sakelley@ucsd.edu>
2018-02-05 15:21:40 -0800

    Some minor post-course adjustments
    Gave the product service the ability to return one
    product by id. Required a little digging into
    Observable in RxJs to understand how to hook that up.
    Confusing at first but very simple in the end.
    Also hooked the star component into the detail page
    and tweaked a few things there, too.
    This is probably done now... will move on to
    additional tutorials or maybe start a project.

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,23 +11,21 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
+  errorMessage: string;
 
   constructor(private _route: ActivatedRoute,
-            private _router: Router) { }
+            private _router: Router,
+          private _productService: ProductService) { }
 
   ngOnInit() {
     let id = +this._route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
-    this.product = {
-      "productId": id,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    }
+    //this.pageTitle += `: ${id}`;
+    console.log("In OnInit for product-detail.component");
+    this._productService.getProductById(id)
+      .subscribe(product => {
+          this.product = product;
+        },
+      error => this.errorMessage = <any>error);
   }
 
   onBack(): void {
--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 
@@ -19,6 +21,11 @@ export class ProductService {
             .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
     }
+    getProductById(id: number): Observable<IProduct> {
+        return  this._http.get<IProduct[]>(this._productUrl)  // get whole array
+            .map(array => array.find(e => e.productId == id)); // 1st element that matches
+    }
+   
     private handleError(err: HttpErrorResponse) {
         console.log(err.message);
         return Observable.throw(err.message);
